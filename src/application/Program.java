b26710a04137a7570e0ea7c767e4edaf8296f1ff
JP <guedesjoaopedro700@gmail.com>
2025-09-23 16:13:37 -0300

    Lógica do checkmate

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,36 +16,36 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
-				UI.clearScreen();
-				UI.printMatch(chessMatch,captured);
+				
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 
 				boolean[][] possibeMovess = chessMatch.possibleMoves(source);
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibeMovess);
 				
+				UI.printBoard(chessMatch.getPieces(), possibeMovess);
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				if(capturedPiece != null) {
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 
 		}
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
